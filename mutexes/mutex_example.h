6b568fe4eaa59dac95c2414461d1fed24bb72ba9
samStan4 <sam.charles.stanley@gmail.com>
2025-01-28 13:33:01 -0800

    added mutexes to race condition

--- mutexes/mutex_example.h
+++ mutexes/mutex_example.h
@@ -8,6 +8,7 @@
 #define MUTEX_EXAMPLE_NUMBER_THREADS 10
 
 struct mutex_example_argument {
+    pthread_mutex_t* lock; // 8 bytes for the pointer to the mutex
     int* count_location; // 8 bytes for the pointer
     int thread_id; // 4 bytes for the thread id
     int count_to; // 4 bytes for the integer
@@ -16,7 +17,9 @@ struct mutex_example_argument {
 void* mutex_example_thread_routine(void* args) {
     struct mutex_example_argument* arg = (struct mutex_example_argument*)args;
     for (int i = 0; i < arg->count_to; ++i) {
+        pthread_mutex_lock(arg->lock);
         ++(*(arg->count_location));
+        pthread_mutex_unlock(arg->lock);
     }
     free(args);
     return NULL;
@@ -25,6 +28,9 @@ void* mutex_example_thread_routine(void* args) {
 void run_mutex_example() {
     const int count_to = 1000;
     int count = 0;
+    pthread_mutex_t mutex;
+
+    pthread_mutex_init(&mutex, NULL);
 
     pthread_t threads[MUTEX_EXAMPLE_NUMBER_THREADS];
 
@@ -33,6 +39,7 @@ void run_mutex_example() {
         arg->count_location = &count;
         arg->count_to = count_to;
         arg->thread_id = i;
+        arg->lock = &mutex;
         if (pthread_create(&threads[i], NULL, &mutex_example_thread_routine, arg) != 0) {
             perror("Error, unable to spawn thread\n");
         }
@@ -43,6 +50,8 @@ void run_mutex_example() {
             perror("Error, unable to join thread\n");
         }
     }
+    
+    pthread_mutex_destroy(&mutex);
 
     const int theoretical_count = MUTEX_EXAMPLE_NUMBER_THREADS * count_to;
 
